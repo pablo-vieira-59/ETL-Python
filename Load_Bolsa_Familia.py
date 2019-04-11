fd7edc4909823d4f3bbe129812aa02e7b7fcd578
Pablo Andrade <46230565+pablo-vieira-59@users.noreply.github.com>
2019-04-11 16:57:32 -0300

    Delete Load_Bolsa_Familia.py

--- Load_Bolsa_Familia.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-dados = pd.read_csv('Dados.csv')
-dados.columns = ['id_mun','nom_mun','uf','regiao','qnt_ben','valor','anomes','semestre','trimestre','bimestre']
-
-dados_dm_local = (dados.groupby(['id_mun','nom_mun','uf','regiao']).size()).get_values
-dados_dm_tempo = (dados.groupby(['anomes','semestre','trimestre','bimestre']).size()).get_values
-dados_ft_dados = (dados[['id_mun','anomes','qnt_ben','valor']]).get_values
